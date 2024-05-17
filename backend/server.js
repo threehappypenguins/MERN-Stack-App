8bf082eb58ed9e0810fa2511d9442ea3bc1d1660
Sarah Poulin <threehappypenguins@gmail.com>
2024-05-17 20:16:29 -0300

    Completed Tutorial #3

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // Express app
 const app = express()
 
 // Middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // Routes
-app.get('/', (req, res) => {
-    res.json({ mssg: 'Welcome to the app' })
-})
+app.use('/api/workouts', workoutRoutes)
 
 // Listen for requests
 app.listen(process.env.PORT,() => {
