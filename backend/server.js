52d3195828e1ea56ff603bc3cd19922baf1a1579
Sarah Poulin <threehappypenguins@gmail.com>
2024-05-18 11:23:39 -0300

    Completed Tutorial #4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // Express app
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/workouts', workoutRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT,() => {
-    console.log('Listening on port 4000')
-})
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() =>{
+        // Listen for requests
+        app.listen(process.env.PORT,() => {
+            console.log('Connected to db & listening on port 4000')
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
