e2cea6402424280a328f943f2136efea7568c753
Sarah Poulin <threehappypenguins@gmail.com>
2024-05-18 18:20:02 -0300

    Completed Tutorial #7

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -39,11 +39,44 @@ const createWorkout = async (req, res) => {
 }
 
 // Delete a workout
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 // Update a workout
+const updateWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,7 +3,9 @@ const express = require('express')
 const {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -18,13 +20,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({ mssg: 'DELETE a new workout' })
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({ mssg: 'UPDATE a new workout' })
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
